7ded63197433365f0ffb71aebf2680f08742f89d
Praneeth Mullapudi <praneeth_mullapudi@thbs.india.com>
2023-12-12 12:38:04 +0530

    Change in cup

--- Test.java
+++ Test.java
@@ -20,7 +20,7 @@ class Test {
 	public static void main(String[] args)
 	{
 
-		int n = 11111;
+		int n = 22322;
 
 		int reverseN = reversNumber(n);
 		System.out.println("Reverse of n = " + reverseN);
